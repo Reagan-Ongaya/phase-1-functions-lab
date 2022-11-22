06eeef69a201a88b8c42789e08dce2a7fcf9ee24
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-11-22 21:27:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(somevalue) {
+    var start = '(38-34)';
+    return 4;
+}
+
+console.log(distanceFromHqInBlocks());
+
+function distanceFromHqInFeet(someValue) {
+    var value = '(38-34)*264';
+    return 1056;
+} 
+console.log(distanceFromHqInFeet ());
+
+function distanceTravelledInFeet(start, destination) {
+    var StartToDestination = '38-34*264';
+    return StartToDestination;
+}
+console.log(distanceTravelledInFeet()); 
+
+function calculatesFarePrice(start, destination) {
+       var StartToDestination = '2500' - ''*(2);
+       var above2500;
+    return 'cannot travel that far';
+}
+
+console.log(calculatesFarePrice() );
+
